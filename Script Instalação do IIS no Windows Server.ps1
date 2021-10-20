8eaea26b048f0f8389112f81771367a17acc17d5
Macnair Rios Calixto <78324420+Macnair-Rios-Calixto@users.noreply.github.com>
2021-10-19 21:55:35 -0300

    Add files via upload

---
+++ Script Instalação do IIS no Windows Server.ps1
@@ -0,0 +1,3 @@
+﻿Install-WindowsFeature -name Web-Server -IncludeManagementTools
+remove-item C:\inetpub\wwwroot\iisstart.htm
+Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Aprendendo Cloud!  " + $env:computername)
